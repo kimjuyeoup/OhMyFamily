575f2804cab0bb048908b9e30c5e88fb2dd6e766
SIWON990327 <pos06058@naver.com>
2024-11-14 03:48:14 +0900

    CI/CD Test

--- src/main/java/com/example/demo/global/health/HealthController.java
+++ src/main/java/com/example/demo/global/health/HealthController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping("/health")
     public String healthCheck() {
-        return "Hello OhMyFamily!";
+        return "Hello OhMyFamily!!!";
     }
 }
