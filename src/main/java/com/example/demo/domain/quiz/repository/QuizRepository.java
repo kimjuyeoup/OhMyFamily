f330b8a2a631b5db81ad576e63045d2e7039d65a
Sayo1593 <sayo1593@nate.com>
2024-12-03 03:16:50 +0900

    에러수정

--- src/main/java/com/example/demo/domain/quiz/repository/QuizRepository.java
+++ src/main/java/com/example/demo/domain/quiz/repository/QuizRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.domain.quiz.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.domain.member.entity.Member;
 import com.example.demo.domain.quiz.entity.Quiz;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-  // List<Quiz> findByUserId(Long userId);
+  List<Quiz> findByMember(Member member);
 }
