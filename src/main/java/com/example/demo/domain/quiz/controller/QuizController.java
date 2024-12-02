0fea4936ab95a192e7137a68ea6355413c652b04
Sayo1593 <sayo1593@nate.com>
2024-12-03 03:16:50 +0900

    에러수정

--- src/main/java/com/example/demo/domain/quiz/controller/QuizController.java
+++ src/main/java/com/example/demo/domain/quiz/controller/QuizController.java
@@ -1,12 +1,20 @@
 package com.example.demo.domain.quiz.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.demo.domain.member.entity.Member;
+import com.example.demo.domain.member.repository.MemberRepository;
 import com.example.demo.domain.quiz.dto.request.SetQuizNicknameRequest;
 import com.example.demo.domain.quiz.dto.response.SetQuizNicknameResponse;
+import com.example.demo.domain.quiz.entity.Quiz;
 import com.example.demo.domain.quiz.repository.QuizRepository;
 import com.example.demo.domain.quiz.service.QuizCommandService;
 import com.example.demo.global.exception.BaseResponse;
+import com.example.demo.global.exception.GlobalErrorCode;
+import com.example.demo.global.exception.GlobalException;
 import com.example.demo.global.jwt.JwtTokenProvider;
 
 import lombok.RequiredArgsConstructor;
@@ -18,7 +26,7 @@ public class QuizController {
 
   private final QuizRepository quizRepository;
   private final JwtTokenProvider jwtTokenProvider;
-
+  private final MemberRepository memberRepository;
   private final QuizCommandService quizCommandService;
 
   @PostMapping("/nickname")
@@ -28,14 +36,20 @@ public class QuizController {
     return BaseResponse.onSuccess(quizCommandService.setQuizNickname(accessToken, request));
   }
 
-  /*@GetMapping("/search")
-  public ResponseEntity<List<Quiz>> getData(@RequestHeader("Authorization") String token) {
-    String accessToken = token.replace("Bearer ", "");
+  @GetMapping("/search")
+  public ResponseEntity<List<Quiz>> getData() {
+    /*public ResponseEntity<List<Quiz>> getData(@RequestHeader("Authorization") String token) {
+      String accessToken = token.replace("Bearer ", "");
+    */
+    // Long userId = Long.parseLong(jwtTokenProvider.extractSubject(accessToken));
+    Long userId = 1L;
+    Member member =
+        memberRepository
+            .findById(userId)
+            .orElseThrow(() -> new GlobalException(GlobalErrorCode.NOT_FOUND_MEMBER));
 
-    Long userId = Long.parseLong(jwtTokenProvider.extractSubject(accessToken));
-
-    List<Quiz> data = quizRepository.findByUserId(userId);
+    List<Quiz> data = quizRepository.findByMember(member);
 
     return ResponseEntity.ok(data);
-  }*/
+  }
 }
