8184db8fbd9f623116f474f0cf137cd2c89d01b8
김시원 / Kim Siwon <114036799+SIWON990327@users.noreply.github.com>
2024-12-22 11:26:44 +0900

    refactor : cors 도메인 추가 (#79)

--- src/main/java/com/example/demo/global/config/WebConfig.java
+++ src/main/java/com/example/demo/global/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/api/**")
-        .allowedOrigins("http://localhost:5173", "https://www.oh-my-family.com")
+        .allowedOrigins(
+            "http://localhost:5173", "https://www.oh-my-family.com", "http://192.168.219.104:5173")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(false)
