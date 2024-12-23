8f4bc7a9c4964672a466a901bd917ac7fb296fc7
Sayo1593 <sayo1593@nate.com>
2024-12-23 19:14:06 +0900

    /**/

--- src/main/java/com/example/demo/domain/Question/service/QuestionServices.java
+++ src/main/java/com/example/demo/domain/Question/service/QuestionServices.java
@@ -12,7 +12,6 @@ import com.example.demo.domain.Question.entity.QuestionEntity;
 import com.example.demo.domain.Question.repository.QuestionRepository;
 import com.example.demo.domain.SetQuestion.entity.SetQuestion;
 import com.example.demo.domain.SetQuestion.repository.SetQuestionRepository;
-import com.example.demo.domain.member.entity.Member;
 import com.example.demo.domain.member.repository.MemberRepository;
 import com.example.demo.domain.quiz.entity.Quiz;
 import com.example.demo.domain.quiz.repository.QuizRepository;
@@ -71,11 +70,11 @@ public class QuestionServices {
 
   public SubmitDto updateSubmitByNickname(SubmitDto submitDto) {
 
-    Long memberId = currentToken.getCurrentMemberId();
+    /*Long memberId = currentToken.getCurrentMemberId();
     Member member =
         memberRepository
             .findById(memberId)
-            .orElseThrow(() -> new IllegalArgumentException("Member not found"));
+            .orElseThrow(() -> new IllegalArgumentException("Member not found"));*/
     List<String> answers = submitDto.getAnswer();
     List<SetQuestion> setQuestions = setQuestionRepository.findAll();
     List<QuestionEntity> questions = new ArrayList<>();
@@ -85,7 +84,7 @@ public class QuestionServices {
       QuestionEntity question = new QuestionEntity();
       question.setName(submitDto.getName());
       question.setAnswer(answers.get(i));
-      question.setMember(member);
+      // question.setMember(member);
       if (i < setQuestions.size()) {
         SetQuestion setQuestion = setQuestions.get(i);
         question.setSetId(setQuestion.getId());
