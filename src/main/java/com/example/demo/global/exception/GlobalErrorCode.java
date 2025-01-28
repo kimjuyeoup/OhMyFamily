09e9543a5e88003af2c740d3e3d6ca65fa79a5ce
김시원 / Kim Siwon <114036799+SIWON990327@users.noreply.github.com>
2025-01-28 13:27:13 +0900

    [Refactor] : 에러 코드 변환 (#118)

--- src/main/java/com/example/demo/global/exception/GlobalErrorCode.java
+++ src/main/java/com/example/demo/global/exception/GlobalErrorCode.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum GlobalErrorCode {
+  ACCESS_DENIED(HttpStatus.FORBIDDEN, "권한이 없습니다."),
+  AUTHENTICATION_REQUIRED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
   NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
   INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다.");
 
