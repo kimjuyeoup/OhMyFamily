ec959b65407db447b1d9b38932be16252c0a7fc5
SIWON KIM <114036799+NCOOL060580327@users.noreply.github.com>
2025-04-25 18:32:34 +0900

    Hotfix/#147 (#149)
    * refactor : CORS WebConfig
    
    * refactor : NGINX 파일 수정

---
+++ src/main/java/com/example/demo/global/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/api/**")
+        .allowedOrigins(
+            "http://localhost:5173",
+            "https://www.oh-my-family.com",
+            "http://192.168.219.104:5173",
+            "https://api.oh-my-family.com")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        .allowedHeaders("Authorization", "Content-Type", "Accept")
+        .allowCredentials(true)
+        .maxAge(6000);
+  }
+}
