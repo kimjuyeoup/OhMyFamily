b84200b85bde1f135509c378e97eefd9022a2059
SIWON KIM <114036799+NCOOL060580327@users.noreply.github.com>
2025-04-26 16:21:41 +0900

    WebConfig 설정 (#159)

--- src/main/java/com/example/demo/global/config/WebConfig.java
+++ src/main/java/com/example/demo/global/config/WebConfig.java
@@ -8,5 +8,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
   @Override
-  public void addCorsMappings(CorsRegistry registry) {}
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/api/**")
+        .allowedOrigins(
+            "http://localhost:5173",
+            "https://www.oh-my-family.com",
+            "http://192.168.219.104:5173",
+            "https://api.oh-my-family.com")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        .allowedHeaders("Authorization", "Content-Type", "Accept")
+        .allowCredentials(true)
+        .maxAge(6000);
+
+    registry
+        .addMapping("/**")
+        .allowedOrigins(
+            "http://localhost:5173",
+            "https://www.oh-my-family.com",
+            "http://192.168.219.104:5173",
+            "https://api.oh-my-family.com")
+        .allowedMethods("OPTIONS")
+        .allowedHeaders("Authorization", "Content-Type", "Accept")
+        .allowCredentials(true)
+        .maxAge(6000);
+  }
 }
