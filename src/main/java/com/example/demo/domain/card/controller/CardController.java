37b30ce0c6e93315b20849118e4ed63bfa18aeb1
SIWON KIM <114036799+NCOOL060580327@users.noreply.github.com>
2025-05-04 20:08:39 +0900

    refactor : search detail 암호화 적용 (#193)

--- src/main/java/com/example/demo/domain/card/controller/CardController.java
+++ src/main/java/com/example/demo/domain/card/controller/CardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jakarta.transaction.Transactional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.domain.card.entity.Card;
 import com.example.demo.domain.card.service.CardCommandService;
 import com.example.demo.domain.quiz.repository.QuizRepository;
+import com.example.demo.global.encrypt.EncryptService;
 import com.example.demo.global.exception.BaseResponse;
 
 @RestController
@@ -20,19 +22,27 @@ public class CardController {
 
   private final CardCommandService cardService;
   private final QuizRepository quizRepository;
+  private final EncryptService encryptService;
 
-  public CardController(CardCommandService cardService, QuizRepository quizRepository) {
+  public CardController(
+      CardCommandService cardService,
+      QuizRepository quizRepository,
+      EncryptService encryptService) {
     this.cardService = cardService;
     this.quizRepository = quizRepository;
+    this.encryptService = encryptService;
   }
 
   @Transactional
   @GetMapping("/search/detail")
-  public BaseResponse<Card> searchDetail(@RequestParam Integer quizid) {
-    Long score = quizRepository.findScoreByQuizid(quizid);
-    String name = quizRepository.findNameByQuizid(quizid);
+  public BaseResponse<Card> searchDetail(
+      @RequestHeader("Authorization") String accessToken, @RequestParam String quizid)
+      throws Exception {
+    Integer id = encryptService.decrypt(quizid).intValue();
+    Long score = quizRepository.findScoreByQuizid(id);
+    String name = quizRepository.findNameByQuizid(id);
     List<Card> cards = cardService.findCardsByScore(score);
-    quizRepository.updateCheck(quizid);
+    quizRepository.updateCheck(id);
 
     Card card = cards.get(0);
     card.setName(name);
